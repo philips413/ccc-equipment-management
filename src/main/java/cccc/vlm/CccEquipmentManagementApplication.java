bc39d2123cfe9cb056300294add40fabae158028
Philips413 <philips413@kakao.com>
2022-04-17 00:20:51 +0900

    create: 프로젝트 생성

---
+++ src/main/java/cccc/vlm/CccEquipmentManagementApplication.java
@@ -0,0 +1,13 @@
+package cccc.vlm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CccEquipmentManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CccEquipmentManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cccc/vlm/CccEquipmentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package cccc.vlm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CccEquipmentManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
